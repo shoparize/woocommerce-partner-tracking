6c2b9c732b0a99a5d9fcbc1ab650a8a5bf50d83c
Umur Alpay <umuralpay@outlook.com>
2022-07-21 14:41:14 +0300

    first commit

---
+++ shoparize.js
@@ -0,0 +1,118 @@
+console.log("Shoparize Plugin INIT");
+const SHOPARIZE_API = () => {
+
+	const shoparize_setToCookies = (cname, cvalue) => {
+    const expire = new Date();
+    const today = new Date();
+    expire.setTime(today.getTime() + 3600000*24*90);
+    document.cookie = cname + "=" + cvalue + ";expires= " + expire.toGMTString() + ";" + ";path=/";
+	}
+	const shoparize_getFromCookies = (cname) => {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+	}
+
+  const sendAsyncReq = (obj, type) => {
+    let xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+
+      //const response = JSON.parse(xmlhttp.responseText);
+      console.log(xmlhttp.responseText)
+
+      if (xmlhttp.readyState == XMLHttpRequest.DONE) { // XMLHttpRequest.DONE == 4
+
+        if (xmlhttp.status == 200) {
+          //Success
+        }else {
+          //Error
+        }
+      }
+    };
+
+    if(type === "click") {
+      xmlhttp.open("POST", "https://partner.shoparize.com/api/incoming/click?x-partner=allow");
+      xmlhttp.setRequestHeader('Content-type', 'application/json');
+      xmlhttp.send(JSON.stringify(obj));
+    }else if(type === "conv") {
+
+      xmlhttp.open("POST", "https://partner.shoparize.com/api/incoming/conv?x-partner=allow");
+      xmlhttp.setRequestHeader('Content-type', 'application/json');
+      xmlhttp.send(JSON.stringify(obj));
+    }
+  }
+  const init = (shopID) => {
+    if(shopID) {
+    
+      const keywords = ["utm_source", "utm_medium", "utm_campaign", "utm_term", "msclkid", "gclid", "wbraid", "gbraid"];
+
+     	const url = new URLSearchParams(window.location.search);
+      //const url = new URLSearchParams("?utm_source=shoparize");
+
+      let isShoparize = false;
+      let obj = {
+      	"shopId": shopID
+      };
+
+      if(url.has('utm_source') && url.get('utm_source').toLowerCase() === "shoparize") {
+        isShoparize = true;
+      }
+
+      if(isShoparize) {
+
+        const epok = new Date().getTime();
+        shoparize_setToCookies("_partner_click_time", epok);
+
+        obj["_partner_click_time"] = epok;
+
+        keywords.forEach(item => {
+          const _found = url.has(item);
+          if(_found) {
+            const val = url.get(item);
+            obj["_partner_" + item] = val;
+            shoparize_setToCookies("_partner_" + item, val);
+          }
+        });
+        
+        sendAsyncReq(obj, 'click');
+      }
+    }else {
+    	throw new Error("Please provide shopID");
+    }
+  }
+  
+  const conv = (shopID) => {
+  	if(shopID) {
+    
+      const keywords = ["_partner_utm_source", "_partner_utm_medium", "_partner_utm_campaign", "_partner_utm_term", "_partner_msclkid", "_partner_gclid", "_partner_wbraid", "_partner_gbraid", "_partner_click_time"];
+
+      let obj = {
+      	"shopId": shopID,
+        "dataLayerShoparize": window.dataLayerShoparize[0] || [],
+        "transaction_time": new Date().getTime()
+      };
+
+      keywords.forEach(item => {
+      
+        obj[item] = shoparize_getFromCookies(item);
+      });
+
+      sendAsyncReq(obj, 'conv');
+    }else {
+    	throw new Error("Please provide shopID");
+    }
+  }
+  return {init: init, conv: conv};
+}
+
---
+++ shoparize.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Plugin Name: Shoparize
+ * Description: This is the plugin that will make me a senior full stack developer and get %30 base salary increase
+ * Plugin URI: https://www.shoparize.com
+ * Version: 0.0.1
+ * Author: Shoparize
+ * Author URI: https://www.shoparize.com 
+ */
+
+// Test to see if WooCommerce is active (including network activated).
+$plugin_path = trailingslashit( WP_PLUGIN_DIR ) . 'woocommerce/woocommerce.php';
+
+if (
+    in_array( $plugin_path, wp_get_active_and_valid_plugins() )
+    || in_array( $plugin_path, wp_get_active_network_plugins() )
+) {
+    // Custom code here. WooCommerce is active, however it has not 
+    // necessarily initialized (when that is important, consider
+    // using the `woocommerce_init` action).
+
+    add_action('wp_enqueue_scripts', 'shoparize_public_scripts');
+
+    function shoparize_public_scripts(){
+ 	wp_enqueue_script( 'shoparize_script', plugin_dir_url( __FILE__ ) . 'shoparize.js' );
+    }
+
+    function after_purchase_action($order) {
+	    //add_action( 'wp_footer', 'add_inline_script', 0 );
+	add_inline_script();
+    }
+    
+    function add_inline_script()
+    {
+	echo '<script>console.log("Order created");const shoparize = SHOPARIZE_API();shoparize.conv("shopID");</script>';
+    }
+
+    add_action('woocommerce_payment_complete', 'after_purchase_action', 10, 1 );
+}
+
+?>
