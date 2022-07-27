ec06e35319ed7f457ee3016485c361681213bcaa
Umur Alpay <umuralpay@outlook.com>
2022-07-27 16:28:25 +0300

    Update shoparize-admin.php

--- shoparize-admin.php
+++ shoparize-admin.php
@@ -1,73 +1,38 @@
 <?php
-/**
- * @internal never define functions inside callbacks.
- * these functions could be run multiple times; this would result in a fatal error.
- */
 
-/**
- * custom option and settings
- */
-function wporg_settings_init() {
-    // Register a new setting for "wporg" page.
-    register_setting( 'wporg', 'shoparize_partner_tracking' );
+function shoparize_partner_settings_init() {
 
-    // Register a new section in the "wporg" page.
+    register_setting( 'shoparize-partner', 'shoparize_partner_tracking' );
+
+
     add_settings_section(
-        'wporg_section_developers',
-        __( 'Shop Settings', 'wporg' ), 'wporg_section_developers_callback',
-        'wporg'
+        'shoparize_partner_dev',
+        __( 'Shop Settings', 'shoparize_partner' ), 'shoparize_partner_dev_callback',
+        'shoparize-partner'
     );
 
-    // Register a new field in the "wporg_section_developers" section, inside the "wporg" page.
     add_settings_field(
-        'wporg_field_pill', // As of WP 4.6 this value is used only internally.
-        // Use $args' label_for to populate the id inside the callback.
-        __( 'Shop ID', 'wporg' ),
-        'wporg_field_pill_cb',
-        'wporg',
-        'wporg_section_developers',
+        'wporg_field_pill',
+        __( 'Shop ID', 'shoparize_partner' ),
+        'shoparize_partner_cb',
+        'shoparize-partner',
+        'shoparize_partner_dev',
         array(
             'label_for'         => 'shop_id',
-            'class'             => 'wporg_row',
+            'class'             => 'shoparize_partner_row',
         )
     );
 }
 
-/**
- * Register our wporg_settings_init to the admin_init action hook.
- */
-add_action( 'admin_init', 'wporg_settings_init' );
+add_action( 'admin_init', 'shoparize_partner_settings_init' );
 
-
-/**
- * Custom option and settings:
- *  - callback functions
- */
-
-
-/**
- * Developers section callback function.
- *
- * @param array $args  The settings array, defining title, id, callback.
- */
-function wporg_section_developers_callback( $args ) {
+function shoparize_partner_dev_callback( $args ) {
     ?>
-    <p id="<?php echo esc_attr( $args['id'] ); ?>"><?php esc_html_e( 'Please enter your shop id.', 'wporg' ); ?></p>
+    <p id="<?php echo esc_attr( $args['id'] ); ?>"><?php esc_html_e( 'Please enter your shop id.', 'shoparize_partner' ); ?></p>
     <?php
 }
 
-/**
- * Pill field callbakc function.
- *
- * WordPress has magic interaction with the following keys: label_for, class.
- * - the "label_for" key value is used for the "for" attribute of the <label>.
- * - the "class" key value is used for the "class" attribute of the <tr> containing the field.
- * Note: you can add custom key value pairs to be used inside your callbacks.
- *
- * @param array $args
- */
-function wporg_field_pill_cb( $args ) {
-    // Get the value of the setting we've registered with register_setting()
+function shoparize_partner_cb( $args ) {
     $options = get_option( 'shoparize_partner_tracking' );
     ?>
     <input
@@ -78,57 +43,35 @@ function wporg_field_pill_cb( $args ) {
     <?php
 }
 
-/**
- * Add the top level menu page.
- */
-function wporg_options_page() {
+function shoparize_partner_options_page() {
     add_menu_page(
-        'Partner Tracking',
-        'Partner Tracking',
+        'Shoparize Partner',
+        'Shoparize Partner',
         'manage_options',
-        'wporg',
-        'wporg_options_page_html'
+        'shoparize-partner',
+        'shoparize_partner_options_page_html'
     );
 }
 
+add_action( 'admin_menu', 'shoparize_partner_options_page' );
 
-/**
- * Register our wporg_options_page to the admin_menu action hook.
- */
-add_action( 'admin_menu', 'wporg_options_page' );
-
-
-/**
- * Top level menu callback function
- */
-function wporg_options_page_html() {
-    // check user capabilities
+function shoparize_partner_options_page_html() {
     if ( ! current_user_can( 'manage_options' ) ) {
         return;
     }
 
-    // add error/update messages
-
-    // check if the user have submitted the settings
-    // WordPress will add the "settings-updated" $_GET parameter to the url
     if ( isset( $_GET['settings-updated'] ) ) {
-        // add settings saved message with the class of "updated"
-        add_settings_error( 'wporg_messages', 'wporg_message', __( 'Settings Saved', 'wporg' ), 'updated' );
+        add_settings_error( 'shoparize_partner_messages', 'shoparize_partner_message', __( 'Settings Saved', 'shoparize_partner' ), 'updated' );
     }
 
-    // show error/update messages
-    settings_errors( 'wporg_messages' );
+    settings_errors( 'shoparize_partner_messages' );
     ?>
     <div class="wrap">
         <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
         <form action="options.php" method="post">
             <?php
-            // output security fields for the registered setting "wporg"
-            settings_fields( 'wporg' );
-            // output setting sections and their fields
-            // (sections are registered for "wporg", each field is registered to a specific section)
-            do_settings_sections( 'wporg' );
-            // output save settings button
+            settings_fields( 'shoparize-partner' );
+            do_settings_sections( 'shoparize-partner' );
             submit_button( 'Save Settings' );
             ?>
         </form>
